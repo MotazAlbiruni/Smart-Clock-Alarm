d7e8880500b7d596a699202dee17e7600022ba09
motaz albiruni <motaz.albiruni@gmail.com>
2021-05-19 00:23:23 +0200

    smart clock edition 3

--- app/src/main/java/com/motazalbiruni/smartclockalarm/deskclock/actionbarmenu/OptionsMenuManager.java
+++ app/src/main/java/com/motazalbiruni/smartclockalarm/deskclock/actionbarmenu/OptionsMenuManager.java
@@ -1,23 +1,8 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.motazalbiruni.smartclockalarm.deskclock.actionbarmenu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -36,7 +21,7 @@ public final class OptionsMenuManager {
     /**
      * Add one or more {@link MenuItemController} to the actionbar menu.
      * <p/>
-     * This should be called in {@link Activity#onCreate(Bundle)}.
+     * This should be called in {@link Activity#onCreate(Bundle, PersistableBundle)}.
      */
     public OptionsMenuManager addMenuItemController(MenuItemController... controllers) {
         Collections.addAll(mControllers, controllers);
